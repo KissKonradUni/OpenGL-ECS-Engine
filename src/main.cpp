8cab3ff61e8d1c53fe656dcaa605c25741d865a2
knehez <knehez@gmail.com>
2022-10-13 21:05:20 +0200

    vcpkg dependency manager added

--- src/main.cpp
+++ src/main.cpp
@@ -160,7 +160,9 @@ int main(int argc, char** argv) {
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-
+#if defined(__APPLE__)
+	glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+#endif
 	// Init window
 	GLFWwindow* window = glfwCreateWindow(resolutionX, resolutionY, windowTitle, fullscreen ? glfwGetPrimaryMonitor() : NULL, NULL);
 	if (window == NULL) {
